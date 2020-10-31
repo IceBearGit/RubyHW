8914dc39a7b852ffa05f4d5fd63a2404be133a2b
Yurii <73390176+IceBearGit@users.noreply.github.com>
2020-10-31 20:15:07 +0200

    solution of the task 1

--- HW1/tasks.rb
+++ HW1/tasks.rb
@@ -1,1 +1,18 @@
-111
+p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.'
+p "even_odd_indexes_parser [1,2,3,4,5,6]"
+
+def even_odd_indexes_parser	(arr)
+
+	puts "elements with even index" 
+	arr.each_with_index do |digit, index|
+  	puts digit if (index % 2) == 0
+	end
+
+	puts "elements with odd index" 
+	arr.each_with_index do |digit, index|
+  	puts digit if (index % 2) != 0
+	end
+end
+p even_odd_indexes_parser [1,2,3,4,5,6]
+p '--------------'
+
