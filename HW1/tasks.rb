40dc5a4af5103e168b811d09bc8ea1bc4a5e5507
Yurii <73390176+IceBearGit@users.noreply.github.com>
2020-10-31 21:32:27 +0200

    solution of the task3

--- HW1/tasks.rb
+++ HW1/tasks.rb
@@ -35,4 +35,33 @@ end
 p odd_even_indexes_parser [1,2,3,4,5,6]
 p '--------------'
 
+p 'Дан целочисленный массив. Вывести вначале все его четные элементы, а затем - нечетные.'
+p "even_odd_parser [1,2,3,4,5,6]"
+def even_odd_parser	(arr)
+
+	arr1 = arr.find_all{ |elem| elem % 2 == 0 }
+	arr2 = arr.find_all{ |elem| elem % 2 != 0 }
+
+	puts "even elements" 
+	p arr1
+
+
+	puts "odd elements" 
+	p arr2
+
+end
+even_odd_parser [1,2,3,4,5,6]
+p '--------------'
+
+
+
+
+
+
+#p 'Дан целочисленный массив. Преобразовать его, прибавив к четным числам первый элемент. Первый и последний элементы массива не изменять.'
+#p 'adding_the_first_element_to_even_numbers [1,2,3,4,5,6]'
+
+#def adding_the_first_element_to_even_numbers (arr)
+
+
 
