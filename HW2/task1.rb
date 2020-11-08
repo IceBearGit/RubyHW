b7535063e9e1528471b3b15a1def2780cf1d1892
Yurii <73390176+IceBearGit@users.noreply.github.com>
2020-11-08 12:50:53 +0200

    task1 v1.0

--- HW2/task1.rb
+++ HW2/task1.rb
@@ -1,1 +1,35 @@
 array = [621, 445, 147, 159, 430, 222, 482, 44, 194, 522, 652, 494, 14, 126, 532, 387, 441, 471, 337, 446, 18, 36, 202, 574, 556, 458, 16, 139, 222, 220, 107, 82, 264, 366, 501, 319, 314, 430, 55, 336]
+p "array = #{array}"
+p array.count
+p '---------------------------'
+p array.reverse
+p '---------------------------'
+p array.max
+p '---------------------------'
+p array.min
+p '---------------------------'
+p array.sort
+p '---------------------------'
+p array.sort { |x, y| y <=> x }
+p '---------------------------'
+p array.reject {|x| x.odd?}
+p '---------------------------'
+p array.find_all {|x| x % 3 == 0 }
+p '---------------------------'
+p array.uniq
+p '---------------------------'
+p array.map { |x| x.to_f / 10 }
+p '---------------------------'
+p array.find_all {|x| (x < 27) && (x > 0)}.map { |x| (x + 9).to_s(36) }
+p '---------------------------'
+array[array.index(array.min)], array[array.index(array.max)] = array.max, array.min
+p array
+p '---------------------------'
+p array[0...array.index(array.min)]
+p '---------------------------'
+def three_min_elements(array)
+  res = array.sort.first 3
+  p res[0..2]
+end
+three_min_elements(array)
+p '---------------------------'
