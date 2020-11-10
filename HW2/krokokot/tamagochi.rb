7fc498b66b742ad4f3e9c9d24b82dc2d40fd7e3c
Yurii <73390176+IceBearGit@users.noreply.github.com>
2020-11-10 21:13:08 +0200

    tanagochi rubocop inspect

--- HW2/krokokot/tamagochi.rb
+++ HW2/krokokot/tamagochi.rb
@@ -1,95 +1,95 @@
-class Pet
+class Pet # class of home pet
   attr_accessor :name, :health, :play, :sleep, :eat, :agress
 
-  def initialize (name)
+  def initialize(name)
     @name = name
-    @health = 100  
+    @health = 100
     @play = 100
-    @sleep = 100 
+    @sleep = 100
     @eat = 100
     @agress = 0
     @need_toilet = false
   end
 
   def treatment
-    if (@health < 100)
+    if @health < 100
       @health = 100
-      puts "Krokokot treating!"
+      puts 'Krokokot treating!'
       puts 'I am feel better!'
-      puts "-------------------"
+      puts '-------------------'
     else
       puts 'I am not ill!'
-      puts "-------------------"
+      puts '-------------------'
       @agress += rand(20)
     end
     @play -= rand(20)
-    @sleep -= rand(20) 
+    @sleep -= rand(20)
     @eat -= rand(20)
-    passageOfTime
+    passage_of_time
   end
 
   def playing
-    if (@agress > 30)
-      puts "Krokokot bit you! Give him some food!"
-      puts "-------------------"
+    if @agress > 30
+      puts 'Krokokot bit you! Give him some food!'
+      puts '-------------------'
     else
       @play = 100
-      print "Krokokot playing!"
-      puts "-------------------"
+      print 'Krokokot playing!'
+      puts '-------------------'
     end
-    @health -= rand(20) 
-    @sleep -= rand(20) 
+    @health -= rand(20)
+    @sleep -= rand(20)
     @eat -= rand(20)
-    passageOfTime
+    passage_of_time
   end
  
- def sleeping
-    if (@sleep < 90)
+  def sleeping
+    if @sleep < 90
       @sleep = 100
-      puts "Krokokot sleeping! zzzzzzz..zzz"
-      puts "-------------------"
+      puts 'Krokokot sleeping! zzzzzzz..zzz'
+      puts '-------------------'
     else
       puts 'I do not want to sleep. Play with me!'
-      puts "-------------------"
+      puts '-------------------'
     end
-    @health -= rand(20) 
+    @health -= rand(20)
     @play -= rand(20)
     @eat -= rand(20)
     @agress -= rand(20)
-    passageOfTime
+    passage_of_time
   end
 
   def feed
-    if (@eat < 100)
+    if @eat < 100
       puts 'nam nam nam!!!'
-      puts "Krokokot eating!"
-      puts "-------------------"
+      puts 'Krokokot eating!'
+      puts '-------------------'
       @eat = 100
       @agress -= rand(20)
     else
       puts 'Krokokot turned away and wagged his tail'
-      puts "-------------------"
+      puts '-------------------'
       @agress += rand(20)
       @need_toilet = true
     end
-    @health -= rand(20) 
+    @health -= rand(20)
     @play -= rand(20)
-    @sleep -= rand(20) 
-    passageOfTime
+    @sleep -= rand(20)
+    passage_of_time
   end
 
   def toilet
     if @need_toilet
       puts 'I have done it'
-      puts "-------------------"
+      puts '-------------------'
       @agress -= rand(20)
       @need_toilet = false
     else
       puts 'I do not want to do this'
-      puts "-------------------"
+      puts '-------------------'
       @agress += rand(20)
     end
-    passageOfTime
+    passage_of_time
   end
 
   def is_dead?
@@ -103,16 +103,16 @@ class Pet
   def cause_of_dead
     if @health <= 0
       @health = 0
-      print "Krokokot #{@name}. Cause of death: disease !!! Game over!"
+      puts "Krokokot #{@name}. Cause of death: disease !!! Game over!"
     elsif @eat <= 0
       @eat = 0
-      print "Krokokot #{@name}. Cause of death: hunger!!! Game over!"
+      puts "Krokokot #{@name}. Cause of death: hunger!!! Game over!"
     elsif @sleep <= 0
       @sleep = 0
-      print "Krokokot #{@name}. Cause of death: insomnia!!! Game over!"
+      puts "Krokokot #{@name}. Cause of death: insomnia!!! Game over!"
     elsif @play <= 0
       @play = 0
-      print "Krokokot #{@name}. Cause of death: boredom!!! Game over!"
+      puts "Krokokot #{@name}. Cause of death: boredom!!! Game over!"
     end
   end
   
@@ -142,14 +142,14 @@ class Pet
     @need_toilet == true
   end
 
-  def passageOfTime
-    p "some hours later"
+  def passage_of_time
+    p 'some hours later'
     puts('I`m so  hungry!') if hungry?
     puts('I`m so boring!') if boring?
     puts('I`m feel bad!') if healthy?
     puts('I need to sleep!') if sleepy?
     puts('I may kill you!') if agressive?
     puts('Pss, where is a toilet here?') if toiletty?
-    puts "------------------------------------------------"
-  end  
+    puts '------------------------------------------------'
+  end
 end
--- HW2/krokokot/ui.rb
+++ HW2/krokokot/ui.rb
@@ -1,69 +1,67 @@
-require_relative 'tamagochi'
+require_relative 'tamagochi' # operated menu for pet
 
 class Ui
   def start
     init_pet
-    
-    puts "------------------------------------------------"
-    puts ("Just born your krokokot " + @pet.name)
-    puts "------------------------------------------------"
+    puts '------------------------------------------------'
+    puts("Just born your krokokot " + @pet.name)
+    puts '------------------------------------------------'
     puts "\nHealth\t\t#{@pet.health}\nEat\t\t#{@pet.eat}\nSleep\t\t#{@pet.sleep}\nPlay\t\t#{@pet.play}"
-    puts ""
-    puts "------------------------------------------------"
-    puts "Krokokot say: "
+    puts ''
+    puts '------------------------------------------------'
+    puts 'Krokokot say: '
     puts "Hi, my name is #{@name}"
     puts 'I am krokokot and your best friend. But if you do not give me a food and play with me, i will eat you :)'
     puts "------------------------------------------------"
-    puts ""
-    puts "-------------------"
+    puts ''
+    puts '-------------------'
     while true
       if @pet.is_dead?
         @pet.cause_of_dead
         break
       elsif @pet.is_owner_dead?
-        puts "Your krokokot killed you. R.I.P."
+        puts 'Your krokokot killed you. R.I.P.'
         break
       end
 
       action = ask_menu
         case action
-        when "1"
-          puts "Selected - 1 "
-          puts "-------------------"
+        when '1'
+          puts 'Selected - 1 '
+          puts '-------------------'
           @pet.treatment
-        when "2"
-          puts "Selected - 2 "
-          puts "-------------------"
+        when '2'
+          puts 'Selected - 2 '
+          puts '-------------------'
           @pet.feed
-        when "3"
-          puts "Selected - 3 "
-          puts "-------------------"
+        when '3'
+          puts 'Selected - 3 '
+          puts '-------------------'
           @pet.sleeping
-        when "4"
-          puts "Selected - 4 "
-          puts "-------------------"
+        when '4'
+          puts 'Selected - 4 '
+          puts '-------------------'
           @pet.playing 
-        when "5"
-          puts "Selected - 5 "
-          puts "-------------------"
+        when '5'
+          puts 'Selected - 5 '
+          puts '-------------------'
           @pet.toilet
-        when "6"
-          puts "Exit programm"
+        when '6'
+          puts 'Exit programm'
           break
-        when ""
-          puts "You do not selected anything!"
-          puts "-------------------"
+        when ''
+          puts 'You do not selected anything!'
+          puts '-------------------'
           @pet.health -= rand(20)
           @pet.eat -= rand(20)
           @pet.sleep -= rand(20)
           @pet.play -= rand(20)
         else
           puts "Вы ввели #{action}, что-то не так!"
-          puts "Try again!"
-          puts "-------------------"
+          puts 'Try again!'
+          puts '-------------------'
         end
       end
-    
   end
 
   def init_pet
@@ -74,26 +72,24 @@ class Ui
   
   def ask_menu
     status_bar
-      print "-------------------"
+      print '-------------------'
       puts "\nWhat do you do with #{@pet.name}:
       1 - Visit a doctor
       2 - Give some food
       3 - Send to sleep
       4 - Play with him
       5 - Toilet for krokokot
-      6 - Exit 
+      6 - Exit
       Press enter if to do nothing..."
-      puts "------------------------------------------------"
+      puts '------------------------------------------------'
       action = gets.chomp().to_s
   end
 
   def status_bar
     puts 'Status'
-    puts "------------------------------------------------"
+    puts '------------------------------------------------'
     puts "\nHealth\t\t#{@pet.health}\nEat\t\t#{@pet.eat}\nSleep\t\t#{@pet.sleep}\nPlay\t\t#{@pet.play}\nAgressive\t#{@pet.agress}"
   end
-
-  
 end
 
 Ui.new.start
